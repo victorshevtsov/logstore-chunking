3ece24caf48473468216794ad0da4b096953611a
Victor Shevtsov <victorshevtsov@gmail.com>
2024-03-19 19:14:06 +0000

    Remove chunking from QueryState

--- src/QeryState.ts
+++ src/QeryState.ts
@@ -1,13 +1,10 @@
 import { MessageID } from "@streamr/protocol";
 import { QueryRef } from "./QueryParams";
 
-const CHUNK_LIMIT = 2;
-
 export class QueryState {
   private _isInitialized: boolean = false;
   private _isFinalized: boolean = false;
   private readonly _messageIds: MessageID[] = [];
-  // private chunkHead: number = 0;
 
   public get isInitialized() {
     return this._isInitialized;
@@ -50,17 +47,6 @@ export class QueryState {
     this._isFinalized = true;
   }
 
-  // public get isChunkReady() {
-  //   return this._messageIds.length - this.chunkHead >= CHUNK_LIMIT ||
-  //     this._isFinalResponseReceived;
-  // }
-
-  // public finishChunk() {
-  //   const head = this.chunkHead;
-  //   this.chunkHead = this._messageIds.length - 1;
-  //   return this._messageIds.slice(head);
-  // }
-
   public shrink(queryRef: QueryRef) {
     let count = 0;
     let index = 0;
@@ -86,16 +72,6 @@ export class QueryState {
 
     if (count > 0) {
       this._messageIds.splice(0, count);
-
-      // if (this._messageIds.length > 0) {
-      //   this._minQueryRef = {
-      //     timestamp: this._messageIds[0].timestamp,
-      //     sequenceNumber: this._messageIds[0].sequenceNumber,
-      //   }
-      // } else {
-      //   this._minQueryRef = undefined;
-      //   this._maxQueryRef = undefined;
-      // }
     }
   }
 }
